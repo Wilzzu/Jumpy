fd55fb5bdd149568ea7348164a89e69dd82ef90b
Wille Karvinen <wille.karvinen@student.lab.fi>
2022-11-24 08:05:00 +0200

    Added aiming mechanic, jumping broken

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Variables for jumping
+    private bool jumpDirectionPhase = false;
+    private bool jumpForcePhase = false;
+    private Rigidbody2D rb;
+    private bool firstTimeLanding = true;
+
+    // Variables for aiming
+    [SerializeField] private Transform aim;
+    [SerializeField] private float rotationSpeed;
+    private bool changeJumpRotation = false;
+    private bool changeJumpForce = false;
+    private IEnumerator jumpForceTimer;
+    private float jumpDirection;
+    private float jumpForce = 0;
+
+
+    // Variables for landing
+    [SerializeField] private int neededLandingTime;
+    private int timeOnPlatform = 999;
+    private IEnumerator platformTimer;
+
+    // Get player rigidbody
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Jumping
+    private void Update()
+    {
+        // Check if player has stood on a platform long enough
+        if (timeOnPlatform >= neededLandingTime)
+        {
+            // Stop timer
+            if (platformTimer != null) StopCoroutine(platformTimer);
+            timeOnPlatform = 0;
+
+            // After this the arrow will start rotating and you can choose jump direction
+            aim.localRotation = Quaternion.Euler(0, 0, -5f);
+            aim.gameObject.SetActive(true);
+            jumpDirectionPhase = true;
+        }
+
+        if (jumpDirectionPhase)
+        {
+            // Rotate arrow
+            float angle = aim.eulerAngles.z;
+            if (angle >= 355) changeJumpRotation = true;
+            if (angle <= 185) changeJumpRotation = false;
+
+            if (changeJumpRotation) aim.Rotate(new Vector3(0, 0, -1 * rotationSpeed) * Time.deltaTime);
+            else aim.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime);
+        }
+
+        // Get jump force
+        if (jumpForcePhase)
+        {
+            if (jumpForce >= 100) changeJumpForce = true;
+            if (jumpForce <= 0) changeJumpForce = false;
+            if (changeJumpForce) jumpForce--;
+            else jumpForce++;
+
+            aim.localScale = new Vector3(jumpForce / 100, 1, 1);
+        }
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            // Set jump force on second jump press and jump
+            if (jumpForcePhase)
+            {
+                rb.AddForce(new Vector2((jumpDirection - 270) * -1, jumpForce * 8));
+                //Vector3 direction = new Vector3((jumpDirection - 270) * -1, 0, 0).normalized;
+                //rb.AddForce(direction * jumpForce, ForceMode2D.Impulse);
+
+                Debug.Log("Dir: " + ((jumpDirection - 270) * -1) + " Force: " + (jumpForce * 8));
+                aim.gameObject.SetActive(false);
+
+                // After jumping reset variables
+                jumpForcePhase = false;
+                timeOnPlatform = 0;
+            }
+
+            // Set a jump direction on first jump press
+            if (jumpDirectionPhase)
+            {
+                jumpDirection = aim.eulerAngles.z;
+                jumpDirectionPhase = false;
+                jumpForcePhase = true;
+            }
+        }
+    }
+
+
+    // Check when player lands on a platform
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (firstTimeLanding)
+        {
+            firstTimeLanding = false;
+            return;
+        }
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            // Start timer to check how long player stands on a platform
+            if (platformTimer != null) StopCoroutine(platformTimer);
+            platformTimer = PlatformTimer();
+            StartCoroutine(platformTimer);
+        }
+    }
+
+    // Check when player leaves a platform
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            // Stop timer and reset landing variables
+            if (platformTimer != null) StopCoroutine(platformTimer);
+            timeOnPlatform = 0;
+        }
+    }
+
+    // Timer for counting how long player has stayed on a platform
+    private IEnumerator PlatformTimer()
+    {
+        int time = 0;
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            time++;
+            Debug.Log("Time: " + time + "/" + neededLandingTime);
+            if (time >= neededLandingTime) Debug.Log("Jumping enabled");
+            timeOnPlatform = time;
+        }
+    }
+
+
+}
