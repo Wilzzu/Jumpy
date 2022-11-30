cef099805294138ec6a708afee0ca30e31454eaa
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2022-11-30 09:31:41 +0200

    Added hasLanded and finally a good camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,39 +4,77 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Variable for main camera
+    // Variable for game objects
     private Camera cam;
+    private Rigidbody2D rb;
+
+    // Variables for camera attributes
+    private float smoothAmount = 0.15f;
+    private IEnumerator smoothTimer;
 
     // Variables for following player
-    [Header("Following player")]
     [SerializeField] private Transform player;
-    [SerializeField] private float smoothAmount;
     private Vector3 velocity = Vector3.zero;
+    private bool moveVert = false;
 
     // Variables for zooming
-    [Header("Zooming")]
-    [SerializeField] private bool zoomed = true;
     [SerializeField] private float zoomSpeed;
+    [SerializeField] private bool zoomed = true;
+    private float[] zoomAmount = new float[] { 8, 7 };
 
     private void Start()
     {
+        // Get important components
         cam = gameObject.GetComponent<Camera>();
+        rb = player.GetComponent<Rigidbody2D>();
     }
 
-    // Check if camera is zoomed and move it
     void FixedUpdate()
     {
-        if (zoomed) MoveCamera(5, 2);
-        else MoveCamera(8, 7);
+        // Assign camera values depending on if player has zoomed or not
+        // Can be used in the future if needed
+        if (zoomed) zoomAmount = new float[] { 5, 3 };
+        else zoomAmount = new float[] { 8, 7 };
+
+        // Move camera vertically when player is moving fast
+        bool playerLanded = player.gameObject.GetComponent<PlayerScript>().hasLanded;
+        if (playerLanded) moveVert = false;
+        else if (Mathf.Abs(rb.velocity.y) > 10 && !moveVert)
+        {
+            // Make camera transition smooth
+            smoothAmount = 0.4f;
+            moveVert = true;
+
+            // Start timer to make camera transition smooth, lerp didn't work for some reason
+            if (smoothTimer != null) StopCoroutine(smoothTimer);
+            smoothTimer = SmoothTimer();
+            StartCoroutine(smoothTimer);
+        }
+
+        // Lastly move camera
+        if (moveVert) MoveCamera(zoomAmount[0], zoomAmount[1] - 2);
+        else MoveCamera(zoomAmount[0], zoomAmount[1]);
     }
 
     // Follow player with camera and apply given zoom amount and offset
-    private void MoveCamera(int zoomAmount, int verticalOffset)
+    private void MoveCamera(float zoomAmount, float verticalOffset)
     {
         Vector3 offset = new Vector3(0, verticalOffset, -10);
-        Vector3 playerPos = player.position + offset;
+        // Vector3 playerPos = player.position + offset; // follow player on x and y axis
+        Vector3 playerPos = new Vector3(0, player.position.y, 0) + offset;
 
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoomAmount, zoomSpeed);
         transform.position = Vector3.SmoothDamp(transform.position, playerPos, ref velocity, smoothAmount);
     }
+
+    // Timer for making camera transitions smooth
+    private IEnumerator SmoothTimer()
+    {
+        while (smoothAmount > 0.15f)
+        {
+            yield return null;
+            smoothAmount -= 0.005f;
+        }
+    }
+
 }
