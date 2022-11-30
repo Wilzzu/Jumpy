8053cf76c757ae0af6b25150020da1b28dcab01f
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2022-11-30 06:47:13 +0200

    Added camera and move jumping to FixedUpdate

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Variable for main camera
+    private Camera cam;
+
+    // Variables for following player
+    [Header("Following player")]
+    [SerializeField] private Transform player;
+    [SerializeField] private float smoothAmount = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
+    // Variables for zooming
+    [Header("Zooming")]
+    [SerializeField] private bool zoomed = true;
+    [SerializeField] private float zoomSpeed;
+
+    private void Start()
+    {
+        cam = gameObject.GetComponent<Camera>();
+    }
+
+    // Check if camera is zoomed and move it
+    void FixedUpdate()
+    {
+        if (zoomed) MoveCamera(5, 2);
+        else MoveCamera(8, 7);
+    }
+
+    // Follow player with camera and apply given zoom amount and offset
+    private void MoveCamera(float zoomAmount, float verticalOffset)
+    {
+        Vector3 offset = new Vector3(0, verticalOffset, -10);
+        Vector3 playerPos = player.position + offset;
+
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoomAmount, zoomSpeed);
+        transform.position = Vector3.SmoothDamp(transform.position, playerPos, ref velocity, smoothAmount);
+    }
+}
