e2c910f534d75ec2b4a15c8b477da4d10efb17d9
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2022-12-02 18:51:44 +0200

    Added mobile inputs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public bool isMobile = false;
     private void Awake()
     {
-
+        // Assign GameManager instance
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
             Destroy(gameObject);
         }
+
+        // Check if mobile
+        if (Application.isMobilePlatform) isMobile = true;
     }
 
     public void LevelFinished()
     {
         Debug.Log("Level finished!");
     }
+
 }
