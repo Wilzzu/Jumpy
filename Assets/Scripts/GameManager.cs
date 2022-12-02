90bd248fb24f8e434222920cb56cee571a35464c
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2022-12-02 19:57:51 +0200

    Added zoomBtn, mobile check, UI toggle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public bool isMobile = false;
+    [SerializeField] private string[] noInGameUIScenes;
+    [SerializeField] private GameObject inGameUI;
+
     private void Awake()
     {
         // Assign GameManager instance
@@ -21,9 +25,32 @@ public class GameManager : MonoBehaviour
         }
 
         // Check if mobile
-        if (Application.isMobilePlatform) isMobile = true;
+        if (Application.isMobilePlatform)
+        {
+            isMobile = true;
+            // Disable PC UI gameObject
+        }
+        else
+        {
+            // Disable Mobile UI gameObject
+        }
     }
 
+    private void OnEnable()
+    {
+        // Subscribe to scene loading
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // Check what scene loaded
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Don't show ingame UI if user is in the menus
+        if (Array.IndexOf(noInGameUIScenes, scene.name) > -1) inGameUI.SetActive(false);
+        else inGameUI.SetActive(true);
+    }
+
+    // When player has finished the level
     public void LevelFinished()
     {
         Debug.Log("Level finished!");
