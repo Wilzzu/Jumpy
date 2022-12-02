c08b265ea5cb154bd075a77fc0bfc503edb39fd9
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2022-12-02 14:57:44 +0200

    Added finish check and GameManager

---
+++ Assets/Dev/DefaultPlayMode.cs
@@ -0,0 +1,29 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class DefaultPlayMode : EditorWindow
+{
+    private string scenePath = "Assets/Scenes/MainMenu.unity";
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Set start Scene: " + scenePath))
+            SetPlayModeStartScene(scenePath);
+    }
+
+    void SetPlayModeStartScene(string scenePath)
+    {
+        SceneAsset startScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(scenePath);
+        if (startScene != null)
+            EditorSceneManager.playModeStartScene = startScene;
+        else
+            Debug.Log("Could not find Scene " + scenePath);
+    }
+
+    [MenuItem("Set Default Scene/Open")]
+    static void Open()
+    {
+        GetWindow<DefaultPlayMode>();
+    }
+}
