cf7c544dce33f5859fb50d295248a965b4f10d72
Wille Karvinen <wille.karvinen@student.lab.fi>
2022-12-05 10:41:55 +0200

    Added level unlocking

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelSelect : MonoBehaviour
+{
+    [SerializeField] private GameObject[] levelBtns;
+
+    public void StartLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void Awake()
+    {
+        for (int i = 0; i < levelBtns.Length; i++)
+        {
+            // Add stats if level is completed
+            if (PlayerPrefs.HasKey(levelBtns[i].name + "_jumps"))
+            {
+                levelBtns[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Completed!";
+                levelBtns[i].transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = "Jumps: " + PlayerPrefs.GetInt(levelBtns[i].name + "_jumps").ToString();
+            }
+
+            if (levelBtns[i].name != "Level_0")
+            {
+                // Unlock levels
+                if (PlayerPrefs.HasKey(levelBtns[i - 1].name + "_jumps"))
+                {
+                    levelBtns[i].GetComponent<Button>().interactable = true;
+                    levelBtns[i].GetComponent<Image>().color = new Color(1, 0.8f, 0.19f);
+                }
+                else
+                {
+                    levelBtns[i].GetComponent<Button>().interactable = false;
+                    levelBtns[i].GetComponent<Image>().color = new Color(0.38f, 0.38f, 0.38f);
+                }
+            }
+        }
+    }
+}
